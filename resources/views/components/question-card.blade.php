202cc90cc8eed76b1d43a8079c018ca221d3ecc4
MahammadNuriyev62 <maganuriyev@gmail.com>
2023-01-03 22:47:31 +0400

    improvements

--- resources/views/components/question-card.blade.php
+++ resources/views/components/question-card.blade.php
@@ -6,17 +6,17 @@
         <th class="border border-slate-700 p-1">{{ $question->body }}</th>
         <th class="border border-slate-700 p-1">{{ $question->type }}</th>
         <th class="border border-slate-700 p-1">
-            <form method="GET" action="/questions/{{ $question->id }}">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold w-full h-full" type="submit">View</button>
-            </form>
+            <a href="/questions/{{ $question->id }}">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold w-full h-full" type="button">View</button>
+            </a>
         </th>
         <th class="border border-slate-700 p-1">
-            <form method="GET" action="/questions/{{ $question->id }}/edit">
-                <button class="bg-amber-600 hover:bg-amber-800 text-white font-bold w-full h-full" type="submit">Edit</button>
-            </form>
+            <a href="/questions/{{ $question->id }}/edit">
+                <button class="bg-amber-600 hover:bg-amber-800 text-white font-bold w-full h-full" type="button">Edit</button>
+            </a>
         </th>
         <th class="border border-slate-700 p-1">
-            <button @click="modal=true; action='/questions/{{$question->id}}'" class="bg-red-500 hover:bg-red-700 text-white font-bold w-full h-full" type="submit">Delete</button>
+            <button @click="modal=true; action='/questions/{{ $question->id }}'" class="bg-red-500 hover:bg-red-700 text-white font-bold w-full h-full" type="button">Delete</button>
         </th>
     </tr>
 </div>
